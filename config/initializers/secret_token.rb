10ebd10e8ae5fbe818364990f3df45c06758a546
Brian Multhauf <brian.multahuf@yahoo.com>
2012-06-20 21:45:27 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Talisman::Application.config.secret_token = '39549dd06594eaec29b38970a9250c2297f6d427c1cfef6799574d6ff3286480ffc9df69f83009f27a65ae72d787138dd37c70db286af1f30041f2d6c0b8abb1'
