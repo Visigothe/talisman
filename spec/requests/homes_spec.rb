437935ceee02894ef7a166685b987c7b11cbae78
Brian Multhauf <brian.multahuf@yahoo.com>
2012-06-20 22:46:43 -0700

    Added tests for the home page. Tests are green.

---
+++ spec/requests/homes_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe "Home" do
+
+	describe "index" do 
+		subject { page }
+		before(:each) { visit root_path }
+
+		it 'should have the right title' do 
+			should have_selector('title', text: full_title(''))
+		end
+
+		it "should have the right heading" do 
+			should have_selector('h1', text: 'Talisman')
+		end
+	end
+end
