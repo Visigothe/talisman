174ae922a2a5c14fd0cc76ddeaeafae5404e16b0
Brian Multhauf <brian.multhauf@gmail.com>
2012-09-15 00:58:07 -0700

    Navbar and Sessions Update
    Updated the navbar and layouts spec to reflect the presence of signin
    and signout links.
    
    Added spec for user sessions. All specs are green.

--- spec/requests/layouts_spec.rb
+++ spec/requests/layouts_spec.rb
@@ -1,13 +1,23 @@
 require 'spec_helper'
 
-describe "Layouts" do
+describe 'Layouts' do
   
   subject { page }
 
-  describe "navbar" do 
+  describe 'navbar' do 
     before { visit root_path }
-    it { should have_selector(".brand", text: 'Talisman') }
+    it { should have_selector('.brand', text: 'Talisman') }
     it { should have_link('Talisman', href: root_path) }
-    it { should have_link('Sign Up', href: new_user_registration_path) }
+
+    describe 'for all users' do
+      it { should have_link('Sign In', href: new_user_session_path) }
+      it { should have_link('Sign Up', href: new_user_registration_path) }
+    end
+
+    describe 'for signed in users' do
+      let(:user) { FactoryGirl.create(:user) }
+      before { confirm_and_signin user }
+      it { should have_link('Sign Out') }
+    end
   end
 end
