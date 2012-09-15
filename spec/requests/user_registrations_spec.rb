93696b38bcd021ecf42eb0870dcbf91438576466
Brian Multhauf <brian.multhauf@gmail.com>
2012-09-14 22:57:25 -0700

    Registrations specs
    Updated registrations specs.
    
    Broke registrations new and edit pages as needed to assure tests
    actually covered what they were supposed to cover.
    
    Minor html update to home index.
    
    Added color to .rspec.

--- spec/requests/user_registrations_spec.rb
+++ spec/requests/user_registrations_spec.rb
@@ -23,7 +23,7 @@ describe 'Registration' do
       it { should have_button('Sign up') }
     end
 
-    context 'failure' do 
+      describe 'failure' do 
       before do
         visit new_user_registration_path
         click_button 'Sign up'
@@ -31,20 +31,20 @@ describe 'Registration' do
       it 'does not create a user' do
         expect { response.should_not change(User, :count) }
       end
-      # Re-renders signup form
+      # Re-renders signup form with error message
       heading_and_title('Sign Up', 'Sign Up')
       it { should have_selector('.alert-error') }
       it { should have_content(I18n.t('simple_form.error_notification.default_message')) }
       # Tests for Devise :validatatble cover the indivual cases for signup failure (i.e. no email)
     end
 
-    context 'success' do 
+    describe 'success' do 
       before { signup user }
       it 'creates a user' do
         expect { response.should change(User, :count).by(1) }
       end
-      # Redirects to root_path
-      # Customized redirect in registrations controller will require change of heading and title
+      # Redirects to root_path with success messgae
+      # Customized redirect in will require change of heading and title
       heading_and_title('Talisman', '')
       it { should have_selector('.alert-success') }
       it { should have_content(I18n.t('devise.registrations.signed_up_but_unconfirmed')) }
@@ -62,23 +62,83 @@ describe 'Registration' do
     end
 
     describe 'form' do 
-      pending 'Fill in the tests for form fields'
+      heading_and_title('Editing', 'Edit Account')
+      it { should have_selector('label', text: 'Email') }
+      it { should have_selector('input#user_email') }
+      it { should have_selector('label', text: 'Password') }
+      it { should have_selector('input#user_password') }
+      it { should have_selector('label', text: 'Password confirmation') }
+      it { should have_selector('input#user_password_confirmation') }
+      it { should have_selector('label', text: 'Current password') }
+      it { should have_selector('input#user_current_password') }
+      it { should have_button('Update') }
+      it { should have_selector('h2', text: 'Cancel my account') }
+      it { should have_link('Cancel my account') }
     end
 
-    context 'failure' do 
-      pending 'Fill in the tests for editing failure'
+    describe 'failure' do
+
+      context 'with missing current password' do
+        before { click_button 'Update' }
+        # Re-renders the Edit form with error message
+        heading_and_title('Editing', 'Edit Account')
+        it { should have_selector('.alert-error') }
+        it { should have_content(I18n.t('simple_form.error_notification.default_message')) }
+      end
+
+      context 'with new password mismatch' do
+        before do
+          fill_in 'Password', with: 'newpassword'
+          fill_in 'Password confirmation', with: 'mismatch'
+          click_button 'Update'
+        end
+        # Re-renders the Edit form with error message
+        heading_and_title('Editing', 'Edit Account')
+        it { should have_selector('.alert-error') }
+        it { should have_content(I18n.t('simple_form.error_notification.default_message')) }        
+      end
     end
 
-    context 'success' do 
-      pending 'Fill in the tests for editing success'
-    end
+    describe 'success' do
 
-    context 'email' do
-      # Redirects to root with success message
-      it "heading_and_title('Talisman', '')"
-      it "{ should have_selector('.alert-success') }"
-      it "{ should have_content(I18n.t('devise.registration.update_needs_confirmation') }"
-      # Tests for reconfirmation email are in confirmations spec     
+      context 'in general' do
+        before do
+          fill_in 'Current password', with: user.password
+          click_button 'Update'
+        end
+        # Re-directs to root path with success message
+        # Customizing the redirect will require changing the heading and title
+        heading_and_title('Talisman', '')
+        it { should have_selector('.alert-success') }
+        it { should have_content(I18n.t('devise.registrations.updated')) }
+      end
+
+      context 'updating password' do
+        before do
+          fill_in 'Password', with: 'newpassword'
+          fill_in 'Password confirmation', with: 'newpassword'
+          fill_in 'Current password', with: user.password
+          click_button 'Update'
+        end
+        # Re-directs to root path with success message
+        # Customizing the redirect will require changing the heading and title
+        heading_and_title('Talisman', '')
+        it { should have_selector('.alert-success') }
+        it { should have_content(I18n.t('devise.registrations.updated')) }
+      end
+
+      context 'updating email' do
+        before do
+          fill_in 'Email', with: 'email@new.com'
+          fill_in 'Current password', with: user.password
+          click_button 'Update'
+        end
+        # Redirects to root with success message
+        heading_and_title('Talisman', '')
+        it { should have_selector('.alert-success') }
+        it { should have_content(I18n.t('devise.registrations.update_needs_confirmation')) }
+        # Tests for reconfirmation email are in confirmations spec
+      end
     end
   end
 
@@ -93,12 +153,13 @@ describe 'Registration' do
 
     describe 'link' do 
 
-      it 'is part of the registration edit form' do
-        pending 'Fill in tests for cancellation link'       
-      end
+      it { should have_link('Cancel my account', ) }
 
       context 'when clicked' do 
-        pending 'Fill in tests for when cancellation link is clicked'
+        before { click_link 'Cancel my account' }
+        it 'deletes the user' do
+          expect { response.should change(User, :count).by(-1) }
+        end
       end
     end
   end  
