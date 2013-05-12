2854b46b3e028f1d977bfe0b5e1d4cc146303328
Brian Multhauf <brian.multhauf@gmail.com>
2013-05-12 15:00:17 -0700

    Major refactor of user_recoverable_spec

--- spec/features/user_recoverable_spec.rb
+++ spec/features/user_recoverable_spec.rb
@@ -1,102 +1,100 @@
 require 'spec_helper'
 
-describe 'Recoverable' do
-  
-  subject { page }
+describe 'Recoverable Module' do
 
-  let(:user) { FactoryGirl.create(:user) }
+  let(:user) { create(:user) }
 
   before { user.confirm! }
 
   describe 'new password page' do
-    before { visit new_user_password_path } 
-    heading_and_title('Forgot your password?', 'Forgotten Password')
+
+    before { visit new_user_password_path }
+
+    subject { page }
+
+    it { should have_title('Forgotten Password') }
+    it { should have_selector('h1', text: 'Forgot your password') }
     it { should have_selector('input#user_email') }
     it { should have_button('Send me reset password instructions') }
-
-    describe 'clicking the reset button' do 
-      before do 
-        fill_in 'Email', with: user.email
-        click_button 'Send me reset password instructions'        
-      end
-      it 'sends an email' do
-        expect { response.should send_email(user.email) }
-      end
-      # Redirects to signin with success message
-      heading_and_title('Sign In', 'Sign In')
-      it { should have_selector('.alert-success') }
-      it { should have_content(I18n.t('devise.passwords.send_instructions')) }
-    end
   end
 
-  describe 'password reset' do
-    before { user.send_reset_password_instructions }
+  describe 'response to clicking "Send me reset password instructions"' do 
 
-    describe 'request' do
-      # Generates reset token and sets reset sent at
-      specify { user.reset_password_token.should_not be_blank }
-      specify { user.reset_password_sent_at.should_not be_blank }
-      it 'sends an email' do
-        expect { response.should send_email(user.email) }
-      end
+    before do
+      Devise::Mailer.deliveries.clear
+      Timecop.freeze
+      visit new_user_password_path
+      fill_in 'Email', with: user.email
+      click_button 'Send me reset password instructions'        
     end
 
-    describe 'email' do 
-      let(:email) { Devise::Mailer.deliveries.last }
-      # Checking for email content
-      specify { email.to.should include(user.email) }
-      it 'is from talisman@example.com' do
-        email.from.should include('talisman@example.com')
-      end
-      it 'has subject' do
-        email.subject.should include(I18n.t('devise.mailer.reset_password_instructions.subject'))
-      end
-      it 'has the user email address in the body' do
-        email.should have_content("Hello #{user.email}")
-      end
-      it 'has content' do
-        email.should have_content('Someone has requested a link to change your password')
-        email.should have_content("If you didn't request this, please ignore this email.")
-        email.should have_content("Your password won't change until you access the link above and create a new one.")
-      end
-      it 'has a link' do
-        email.should have_link('Change my password')
-      end
+    subject { user.reload }
+
+    its(:reset_password_token) { should_not be_nil }
+    its(:reset_password_sent_at) { should eq Time.now }
+    specify { Devise::Mailer.deliveries.count.should eq 1 }
+    specify { current_path.should eq new_user_session_path }
+    specify { page.should have_selector('.alert-success', text: I18n.t('devise.passwords.send_instructions')) }
+  end
+
+  describe 'password reset page' do
+    before do
+      user.send_reset_password_instructions
+      visit edit_user_password_path(reset_password_token: user.reset_password_token)
     end
 
-    describe 'page' do 
-      before { visit "http://localhost:3000/users/password/edit?reset_password_token=#{user.reset_password_token}" }
-      heading_and_title('Change your password', 'Reset Password')
-      it { should have_selector('input#user_password') }
-      it { should have_selector('input#user_password_confirmation') }
-      it { should have_button('Change my password') }
+    subject { page }
+
+    it { should have_title('Reset Password') }
+    it { should have_selector('h1', text: 'Change your password') }
+    it { should have_selector('input#user_password') }
+    it { should have_selector('input#user_password_confirmation') }
+    it { should have_button('Change my password') }
+  end
+
+  describe 'response to password reset' do
+
+    before do
+      user.send_reset_password_instructions
+      visit edit_user_password_path(reset_password_token: user.reset_password_token)
     end
 
-    describe 'failure' do 
+    context 'failure' do 
+      before { click_button 'Change my password' }
+
+      specify { current_path.should eq user_password_path }
+      specify { page.should have_selector('.alert-error', text: I18n.t('simple_form.error_notification.default_message')) }
+      specify { expect { user_signed_in?.should be_false } }
+    end
+
+    context 'success' do
+
       before do 
-        visit "http://localhost:3000/users/password/edit?reset_password_token=#{user.reset_password_token}"
-        click_button 'Change my password'
-      end
-      # Re-renders the reset password page with error message
-      heading_and_title('Change your password', 'Reset Password')
-      it { should have_selector('.alert-error') }
-      it { should have_content(I18n.t('simple_form.error_notification.default_message'))}
-    end
-
-    describe 'success' do 
-      before do 
-        visit "http://localhost:3000/users/password/edit?reset_password_token=#{user.reset_password_token}"
         fill_in 'New password', with: 'newpassword'
         fill_in 'Confirm your new password', with: 'newpassword'
         click_button 'Change my password'
       end
-      # Redirects to root with success message and signs in the user
-      heading_and_title('Talisman', '')
-      it { should have_selector('.alert-success') }
-      it { should have_content(I18n.t('devise.passwords.updated')) }
-      specify 'logs the user in' do 
-        expect { user_signed_in?.should be_true }
-      end
+
+      specify { current_path.should eq root_path }
+      specify { page.should have_selector('.alert-success', text: I18n.t('devise.passwords.updated')) }
+      specify { expect { user_signed_in?.should be_true } }
     end
   end
+
+  describe 'new password email' do
+    # TODO: Move this to a more appropriate spot, perhaps devise_email_spec.rb?
+
+    before do
+      Devise::Mailer.deliveries.clear
+      user.send_reset_password_instructions
+    end
+
+    subject { Devise::Mailer.deliveries.last }
+
+    its(:to) { should include(user.email) }
+    its(:from) { should include('talisman@example.com') }
+    its(:subject) { should include(I18n.t('devise.mailer.reset_password_instructions.subject')) }
+    its(:body) { should have_link('Change my password') }
+  end
+
 end
