01f32d07984bde3df173868622e27f72cc706e8f
Brian Multhauf <brian.multhauf@gmail.com>
2013-05-12 18:30:40 -0700

    Refactor user_rememberable_spec.rb

--- spec/features/user_rememberable_spec.rb
+++ spec/features/user_rememberable_spec.rb
@@ -1,14 +1,15 @@
 require 'spec_helper'
 
-describe 'Rememberable' do
+describe 'Rememberable Module' do
+  
+  let(:user) { create(:user) }
   
   subject { page }
-  let(:user) { FactoryGirl.create(:user) }
+
   before { visit new_user_session_path }
 
-  describe 'remember me checkbox' do 
-    it { should have_selector('.control-label', text: 'Remember me') }
-    it { should have_selector('label.checkbox') }
+  describe 'remember me checkbox on signin page' do 
+
     it { should have_selector('input#user_remember_me') }
     it 'is not checked' do
       remember_me = find('input#user_remember_me')
@@ -22,26 +23,18 @@ describe 'Rememberable' do
   end
 
   describe 'after login' do
-    before do
-      check('Remember me')
-      confirm_and_signin user
-    end
-    it 'sets remember_created_at' do 
-      expect { user.remember_created_at.should_not be_nil }
-    end
-    # Change test to reflect the desired length of time before doing so in config
-    specify 'remember_created_at expires in 2 weeks' do
-      expect { user.remember_expires_at.should == user.remember_created_at + 2.weeks }
-    end
-  end
 
-  describe 'forget me' do 
-    specify 'link' do
-      pending # Fill in tests for links/buttons on specific pages before implementing
+    before do
+      Timecop.freeze
+      check('user_remember_me')
+      fill_in 'user_email', with: user.email
+      fill_in 'user_password', with: user.password
+      click_button 'Sign in'
     end
-    it 'removes remember_created_at' do
-      user.forget_me!
-      user.remember_created_at.should be_nil
-    end
-  end 
+
+    subject { user.reload }
+
+    its(:remember_created_at) { should eq Time.now }
+    its(:remember_expires_at) { should eq subject.remember_created_at + 2.weeks }
+  end
 end
